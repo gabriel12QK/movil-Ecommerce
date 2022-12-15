e3e60b2549a269220971bf690d74eabb2351c89e
gabriel12QK <jose.rivas@espam.edu.ec>
2022-12-15 14:25:51 -0500

    login

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MenuController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  form!:FormGroup
+  state:any=true;
+  constructor(
+      public formbuilder:FormBuilder,
+      private usuarioService:UsuarioService,
+      private menu: MenuController,
+      private router: Router,
+      private toastController: ToastController
+      ) {
+    this.construirForm();
+   }
+  get getControls() {
+    return this.form.controls
+   }
+    construirForm(){
+      this.form=this.formbuilder.group({
+        email:['',[Validators.required]],
+        password:['',[Validators.required,Validators.maxLength(15), Validators.minLength(8)]]
+      })
+
+    }
+  ngOnInit() {
+  }
+  login(){
+    if(this.form.valid){
+      this.usuarioService.Login(this.form.value).subscribe({
+        next:(res)=>{
+          this.usuarioService.saveToken(res);
+          this.toast(res.message)
+         // this.router.navigate(['home']);
+        },
+        error:(err)=> this.toast(err.error.message)
+      });
+    }else{
+        this.state=false;
+    }
+  }
+  ionViewWillEnter() {
+    this.menu.enable(false);
+  }
+  async toast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+    });
+    toast.present();
+  }
+}
--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient,HttpParams} from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import { NavParams } from '@ionic/angular';
+import { formatDate } from '@angular/common';
 
 
 @Injectable({
@@ -30,6 +31,21 @@ export class UsuarioService {
     params.set('telefono',form.telefono);
 
     return this.http.post<any>(`${environment.urlApi}registro`,params)
+  }
+
+  Login(form:any){
+    const params = new FormData();
+    params.set('email',form.email);
+    params.set('password',form.password)
+    return this.http.post<any>(`${environment.urlApi}login`,params);
+  }
+  saveToken(token: any) {
+    localStorage.setItem('id', token.id.toString());
+    localStorage.setItem('typeUserId', token.typeUserId.toString());
+    localStorage.setItem('unver', token.userName);
+    localStorage.setItem('email', token.email);
+    localStorage.setItem('token', token.accesToken);
+    localStorage.setItem('rol', token.rol);
   }
 
 }
